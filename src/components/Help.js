23b6b2eef7267ccc230af7e894f5f582fa058499
Kyle George <k.george21@outlook.com>
2019-03-31 14:27:54 -0400

    structure help page

--- src/components/Help.js
+++ src/components/Help.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 export default class Help extends Component {
     render() {
         return(
-            <div className='help-form'>
-                <form>
-                    <input type='text' name='subject'/>
+            <div className='form-container'>
+            <div className='form'>
+                <form className='help-form'>
+                    <h4>Subject</h4>
+                    <input type='text' name='subject' className='input'/>
                     <br></br>
-                    <textarea></textarea>
+                    <h4>Body</h4>
+                    <textarea type='text' name='body' className='textarea'/>
+                    <br></br>
+                    <input type='submit' className='submit'/>
                 </form>
+            </div>
             </div>
         );
     }
