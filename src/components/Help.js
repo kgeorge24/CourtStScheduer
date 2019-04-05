967e4ed6e0933980d620cc3b17785c8d039c2f84
Kyle George <k.george21@outlook.com>
2019-04-05 00:40:51 -0400

    updated help page

--- src/components/Help.js
+++ src/components/Help.js
@@ -6,13 +6,17 @@ export default class Help extends Component {
             <div className='form-container'>
             <div className='form'>
                 <form className='help-form'>
+                    <h2>Report an Issue
+                        <br></br>
+                        <br></br>
+                    </h2>
                     <h4>Subject</h4>
                     <input type='text' name='subject' className='input'/>
                     <br></br>
                     <h4>Body</h4>
                     <textarea type='text' name='body' className='textarea'/>
                     <br></br>
-                    <input type='submit' className='submit'/>
+                   <button className='submit'>Submit</button>
                 </form>
             </div>
             </div>
